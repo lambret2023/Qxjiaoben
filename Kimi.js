baac8dfe1d28ff98502835c235c46fa2bb34eeeb
lambret2023 <135798479+lambret2023@users.noreply.github.com>
2023-06-07 15:41:26 +0800

    Create Kimi.js
    Kimi会员

---
+++ Kimi.js
@@ -0,0 +1,28 @@
+# Qxjiaoben
+{
+  "cmdid" : "P311",
+  "errorcode" : "0",
+  "rs" : {
+    "r" : [
+      {
+        "userviptype" : "0",
+        "uservipexpiretime" : "0",
+        "age" : "28",
+        "userid" : "308850712",
+        "useravatarurlnormal" : "https://filedownload.ktvme.com/FileService/downloadstorefile.do?fileid=982876043",
+        "useridiograph" : "",
+        "usergender" : "0",
+        "useravatarurl" : "https://filedownload.ktvme.com/FileService/downloadstorefile.do?fileid=982876031",
+        "useravatarurllarge" : "https://filedownload.ktvme.com/FileService/downloadstorefile.do?fileid=982876031",
+        "username" : "7889647bcf3c7f2e8",
+        "useravatarurlsmall" : "https://filedownload.ktvme.com/FileService/downloadstorefile.do?fileid=982876045",
+        "usernickname" : "LUOMR.",
+        "userhobby" : "",
+        "userbirthday" : "0",
+        "status" : "0",
+        "userphonenumber" : ""
+      }
+    ]
+  },
+  "errormessage" : "success"
+}
